8d4137497d063960bb4dbfd8beff4f9ebb99ce34
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-12-23 23:48:42 +0300

    add layout and props childs

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Routes,Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-  <div>
-    <MainNavigation />
+  <Layout>
     <Routes>
       <Route path="/" element={<AllMeetupsPage />} exact />
       <Route path="/new-meetup" element={<NewMeetupPage />} />
       <Route path="/favorites" element={<FavoritesPage />} />
     </Routes>
-  </div>
+  </Layout>
   );
 }
 
