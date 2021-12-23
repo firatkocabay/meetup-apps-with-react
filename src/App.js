746e335f94eea177f72abfed7d3da62d5270ed21
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-12-23 22:45:46 +0300

    update main navigation with style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Routes,Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
   <div>
+    <MainNavigation />
     <Routes>
       <Route path="/" element={<AllMeetupsPage />} exact />
       <Route path="/new-meetup" element={<NewMeetupPage />} />
-      <Route path="/favs" element={<FavoritesPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
     </Routes>
   </div>
   );
