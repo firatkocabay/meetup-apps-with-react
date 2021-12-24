be9ab6f4be90fa226fff9836700d1113fd666096
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-12-24 23:03:47 +0300

    add new meetups using altogic backend

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,51 @@
 import classes from './NewMeetupForm.module.css';
 import Card from "../ui/Card";
+import { useRef } from 'react';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+    const titleInput = useRef();
+    const imageInput = useRef();
+    const addressInput = useRef();
+    const descriptionInput = useRef();
+
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        const currentTitle = titleInput.current.value;
+        const currentImage = imageInput.current.value;
+        const currentAddress = addressInput.current.value;
+        const currentDescription = descriptionInput.current.value;
+
+        const meetupData = {
+            title: currentTitle,
+            image: currentImage,
+            address: currentAddress,
+            description: currentDescription,
+        };
+
+        console.log(meetupData);
+        props.onAddNewMeetup(meetupData);
+    }
+
     return (
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor='title'>Meetup Title</label>
-                    <input type='text' required id='title' />
+                    <input type='text' required id='title' ref={titleInput} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='image'>Meetup Image</label>
-                    <input type='url' required id='image' />
+                    <input type='url' required id='image' ref={imageInput} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='address'>Address</label>
-                    <input type='text' required id='address' />
+                    <input type='text' required id='address' ref={addressInput} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='description'>Description</label>
-                    <textarea id='description' required rows='5'></textarea>
+                    <textarea id='description' required rows='5' ref={descriptionInput}></textarea>
                 </div>
                 <div className={classes.actions}>
                     <button>Add Meetup</button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addNewMeetupHandler(meetup) {
+        fetch(
+            'https://dev001.na-dev-engine.altogic.com/e:61c6266a49b7b99f76f2fb95/meetup',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetup),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddNewMeetup={addNewMeetupHandler} />
         </section>
     );
 }
