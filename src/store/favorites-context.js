7a26d0a729ae193edd0ddda25383f25d8345bb7b
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-12-26 00:17:46 +0300

    add favorite contexts

---
+++ src/store/favorites-context.js
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+    favorites: [],
+    totalFavorites: 0,
+});
+
+function FavoritesContextProvider(props) {
+    const [userFavorites, setUserFavorites] = useState([]);
+
+    function addFavoriteHandler(favoriteMeetup) {
+        setUserFavorites((prevFavorites) => {
+            return prevFavorites.concat(favoriteMeetup);
+        });
+    }
+
+    function removeFavoriteHandler(meetupId) {
+        setUserFavorites(prevFavorites => {
+            return prevFavorites.filter(meetup =>  meetup.id !== meetupId);
+        })
+    }
+
+    function itemIsFavoriteHandler(meetupId) {
+        return userFavorites.some(meetup => meetup.id === meetupId)
+    }
+    
+    const context = {
+        favorites: userFavorites,
+        totalFavorites: userFavorites.length,
+    };
+    
+    retur (
+        <FavoritesContext.Provider value={context}>
+            {props.children}
+        </FavoritesContext.Provider>
+    )
+}
