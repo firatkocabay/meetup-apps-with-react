25812c6f28115968342ae9c25445539d3f1861f7
Fırat Kocabay <12109835+firatkocabay@users.noreply.github.com>
2021-12-25 23:45:14 +0300

    use real data with useEffect

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -6,8 +6,8 @@ function MeetupList(props) {
     <ul className={classes.list}>
         {props.meetups.map((meetup) => (
         <MeetupItem 
-            key={meetup.id} 
-            id={meetup.id} 
+            key={meetup._id} 
+            id={meetup._id} 
             image={meetup.image}
             title={meetup.title}
             address={meetup.address}
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,30 +1,32 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is İstanbul meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Istanbul_siluet.jpg/1280px-Istanbul_siluet.jpg',
-      address: 'Meetupstreet 5, 12345 İstanbul City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is İzmir meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Izmir_Konak_Square.jpg/1280px-Izmir_Konak_Square.jpg',
-      address: 'Meetupstreet 5, 12345 İzmir City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-  ];
+import { useState, useEffect } from 'react';
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetupData, setMeetupData] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://dev001.na-dev-engine.altogic.com/e:61c6266a49b7b99f76f2fb95/meetup'
+      ).then((response) => {
+        return response.json();
+      }).then((data) => {
+        const meetups = data.result;
+        setIsLoading(false);
+        setMeetupData(meetups);
+      });
+  }, []);
+
+
+  if (isLoading) {
+    return <section>
+      <h3>Loading...</h3>
+    </section>
+  }
     return <section>
         <h1>All Meetups</h1>
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={meetupData} />
         </section>;
 }
 
